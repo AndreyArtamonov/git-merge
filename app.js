4f140e5e8a9231e0704e62992dc09291b059d4dc
Andrey Artamonov <bizgrof@gmail.com>
2023-09-10 12:27:36 +0300

    Лучший коммит в жизни

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     let element = document.querySelector('.block');
-    let counter = 1;
+    let  counter = 1;
 
     setInterval(() => {
-        element.style.background = element.style.background === 'red' ? 'green' : 'red';
+        element.style.background = element.style.background === 'red' ? 'brown' : 'red';
         element.style.background = 'red';
 
         if(counter < 100) {
@@ -11,3 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }, 1000)
 });
+
+qwdoqwidoqwdq
+wodqiwdqwo
+dqwd
